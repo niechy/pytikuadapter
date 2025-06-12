bc5fb9b79b6ff14dc6e8b5b0c0924e7e7a5f7d7b
上杉えりい <47024820+niechy@users.noreply.github.com>
2025-06-12 23:00:05 +0800

    feat(adapter): 添加 Zero 适配器 目前还有问题，按道理对方服务器应当返回application/json

---
+++ adapter/zero.py
@@ -0,0 +1,32 @@
+from models import AdapterAns, ErrorType, Srequest
+from core import Adapter
+
+
+class Zero(Adapter):  # pylint: disable=too-few-public-methods
+    url: str = "http://www.anyzero.org/user/api.php"
+    FREE = True
+    PAY = True
+    header = {"Content-Type": "application/json"}
+
+    async def search(self, question: Srequest):
+        params = {
+            "token": question.use["Zero"].token,
+            "q": question.question
+        }
+        async with self.session.get(url=self.url, headers=self.header, params=params) as response:
+            ans: AdapterAns = AdapterAns(None, question.type, None)
+            ans.answer = []
+            print(await response.text())
+            if response.status == 200:
+                req = await response.json()
+                # 这段代码会报错，因为对方服务器返回Content-Type为text/html，所以无法解析成json
+                if req["code"] == 1:
+                    # for i in req["data"]["answer"]:
+                    #     ans.answer.append(question.options[self.OPTION[i]])
+                    ans.answer = req["data"]["answer"]
+                    print(ans.answer)
+                else:
+                    ans.error = ErrorType.TARGET_NO_ANSWER
+            else:
+                ans.error = ErrorType.TARGET_SERVER_ERROR  # 对方服务器有猫病
+            return ans
